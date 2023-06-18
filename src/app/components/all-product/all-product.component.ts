d9938e9c712ee1c4fbe6ed260f2f060fc9cc0f97
tiennq <tiennqhe153125@fpt.edu.vn>
2023-06-18 12:12:05 +0700

    All products

--- src/app/components/all-product/all-product.component.ts
+++ src/app/components/all-product/all-product.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/Product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-all-product',
   templateUrl: './all-product.component.html',
   styleUrls: ['./all-product.component.css']
 })
-export class AllProductComponent {
+export class AllProductComponent implements OnInit{
+  all_products!: Product[];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe((data: Product[]) => {
+      console.log(data);
+      this.all_products = data;
+    });
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -11,6 +11,7 @@ export class ProductService {
   private trending = "http://localhost:8081/trending-products";
   private bestSeller = "http://localhost:8081/bestseller-products";
   private highlight = "http://localhost:8081/highlight-products";
+  private all = "http://localhost:8081/all-products";
 
   constructor(private http: HttpClient) {
     
@@ -26,5 +27,9 @@ export class ProductService {
 
   getHighlightProducts(): Observable<Product[]> {
     return this.http.get<Product[]> (`${this.highlight}`);
+  }
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]> (`${this.all}`);
   }
 }
